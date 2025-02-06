7e753fcc7323ec52ec989636851d8d1ebad356f3
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2025-02-06 14:39:33 -0300

    Update sub.test.js

--- sub.test.js
+++ sub.test.js
@@ -1,7 +1,7 @@
 const sub = require('./sub')
 
 test('A função soma deve retornar 5 ao somar 2 e 3' , () => {
-    expect(sub(3,2)).toBe(1)
+    expect(sub(2,3)).toBe(-1)
 })
 
 test('A função soma deve soma e deve retornar 2 ao somar 1 e 1',() => {
