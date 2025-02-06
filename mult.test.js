da0bf9234df242162116313b0539b50dc8feac4e
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2025-02-06 14:41:16 -0300

    Update mult.test.js

--- mult.test.js
+++ mult.test.js
@@ -1,7 +1,7 @@
 const mult = require('./mult')
 
 test('A função soma deve retornar 5 ao somar 2 e 3' , () => {
-    expect(mult(2,3)).toBe(6)
+    expect(mult(2.5,3)).toBe(7.5)
 })
 
 test('A função soma deve soma e deve retornar 2 ao somar 1 e 1',() => {
